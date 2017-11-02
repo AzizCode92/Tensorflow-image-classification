d0054c44516bed2fbc2f67632307e130ca3ee34b
AzizCode92 <azizbenothman76@gmail.com>
2017-11-03 00:49:31 +0100

    Update label_image.py

--- label_image.py
+++ label_image.py
@@ -11,10 +11,10 @@ image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
 # Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line 
-                   in tf.gfile.GFile("retrained_labels.txt")]
+                   in tf.gfile.GFile("./retrained_labels.txt")]
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+with tf.gfile.FastGFile("./retrained_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
@@ -27,7 +27,7 @@ with tf.Session() as sess:
              {'DecodeJpeg/contents:0': image_data})
     
     # Sort to show labels of first prediction in order of confidence
-    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::]
     
     for node_id in top_k:
         human_string = label_lines[node_id]
